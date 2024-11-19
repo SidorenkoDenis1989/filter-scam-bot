4615be1dba6505a72e8892b8491c8a8b0c02ad63
Denys Sydorenko <n01575559@unf.edu>
2024-11-19 17:32:35 -0500

    Implement test bot

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
-require('dotenv').config();  // Get the environment variables
-const { bot } = require('./bot');
+const TelegramBot = require('node-telegram-bot-api');
+require('dotenv').config();
 
-console.log('Bot is running...');
+const token = process.env.BOT_TOKEN;
+const bot = new TelegramBot(token, { polling: true });
+
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, `Hi, I'm your test bot!`);
+});
+
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+  const text = msg.text;
+
+  if (text) {
+    bot.sendMessage(chatId, `You have written: ${text}`);
+  }
+});
